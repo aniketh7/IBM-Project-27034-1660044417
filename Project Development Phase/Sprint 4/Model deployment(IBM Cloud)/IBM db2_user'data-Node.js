e1a65270c96b30cf71f873a25f65c72ad36cdc8c
Kamalesh S <kamalesh.sselvaraj@gmail.com>
2022-11-16 20:28:41 +0530

    Create IBM db2_user'data-Node.js

---
+++ Project Development Phase/Sprint 4/Model deployment(IBM Cloud)/IBM db2_user'data-Node.js
@@ -0,0 +1,28 @@
+var http = require("https");
+
+var options = {
+  "method": "POST",
+  "hostname": "{REST_API_HOSTNAME}",
+  "port": null,
+  "path": "/dbapi/v4/auth/tokens",
+  "headers": {
+    "content-type": "application/Parkinson_MLmodel.sav",
+    "x-deployment-id": "D6717965d-bf1b-492f-9374-b9791915c168"
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.write(JSON.stringify({ userid: '<ADD STRING VALUE>', password: '<ADD STRING VALUE>' }));
+req.end();
